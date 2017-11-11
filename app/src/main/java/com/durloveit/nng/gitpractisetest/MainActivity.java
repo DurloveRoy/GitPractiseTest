623b5c99a703cc7360b9faa4a9d062fadc1ab051
durloveit <roy2bd@yahoo.com>
2017-11-11 10:51:01 +0600

    1st Committe

--- app/src/main/java/com/durloveit/nng/gitpractisetest/MainActivity.java
+++ app/src/main/java/com/durloveit/nng/gitpractisetest/MainActivity.java
@@ -2,6 +2,7 @@ package com.durloveit.nng.gitpractisetest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public  void GitTestMethod()
+    {
+        Toast.makeText(getApplicationContext(), "Test", Toast.LENGTH_LONG).show();
     }
 }
